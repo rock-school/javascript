60fdc630546d06f269a0fc5ac10a64d4da0ba00d
romanlavrinjuk <romanrostislavovich@gmail.com>
2023-09-26 12:31:00 +0200

    update array

--- js/0300-array/array.js
+++ js/0300-array/array.js
@@ -10,7 +10,7 @@ FUCK UP
 new Array(3) // [undefined, undefined, undefined]
 new Array("3") // ["3']
  */
-
+/*
 const array = [1, 3, 2];
 const arrayInArray = [ [1, 2], [3, 4] ]
 const objectsInArray = [ { 'prop': 1 }, { 'prop': 2 } ]
@@ -68,5 +68,99 @@ Array.map((item, index, array) => item * 2)  // [2, 6, 4]
 array.forEach((item, index, array) => {
   console.log(item);
 })
+*/
+
+const array = [1, 3, 2];
+const arrayInArray = [
+  [1, 2],
+  [3, 4]
+]
+const objectsInArray = [
+  { 'prop': 1, 'another-prop': 3 },
+  { 'prop': 2 }
+]
+
+// AT
+console.log(`Array at [-1]:`, array[- 1])
+console.log(`Array at at(-1):`, array.at(-2));
+
+// Concat
+console.log(`Concat 2 array: `,array.concat(arrayInArray, objectsInArray));
+
+// Every
+console.log(`Every array: `,array.every((item) => item < 10));
+
+// Entries
+
+// Fill
+
+// Filter
+console.log(`Filter array:`, array.filter((item, index, array) => item > 2));
+
+// Find
+console.log(`Find array:`, objectsInArray.find((item, index, array) => item.prop === 1));
+// Array.prototype.findIndex()
+// Array.prototype.findLast()
+// Array.prototype.findLastIndex()
+
+// forEach
+console.log(`ForEach array:`)
+array.forEach((item, index, array) => {
+   console.log(item)
+});
+
+// includes
+console.log(`Includes array with number:`, array.includes(2));
+console.log(`Includes array with object:`, objectsInArray.includes({ prop: 2}));
+
+// join
+console.log(`Join array:`, array.join(''));
+console.log(`Example str: `, 'abcd'.split('').reverse().join(''));
+
+// map
+console.log(`Map array:`, array.map((item, index, array) => {
+  if (item === 3) {
+    return item * 2
+  }
+  return  item;
+}));
+
+/*
+Редко но полезно
+Array.prototype.entries()
+Array.prototype.fill()
+Array.prototype.flat()
+Array.prototype.flatMap()
+
+Редко используется
+Array.prototype.copyWithin()
 
 
+
+Array.from()
+Array.fromAsync()
+Array.prototype.indexOf()
+Array.isArray()
+Array.prototype.keys()
+Array.prototype.lastIndexOf()
+
+Array.of()
+Array.prototype.pop()
+Array.prototype.push()
+Array.prototype.reduce() +
+Array.prototype.reduceRight()
+Array.prototype.reverse() +
+Array.prototype.shift()
+Array.prototype.slice()+
+Array.prototype.some()+
+Array.prototype.sort()+
+Array.prototype.splice()
+Array.prototype.toLocaleString()
+Array.prototype.toReversed()
+Array.prototype.toSorted()
+Array.prototype.toSpliced()
+Array.prototype.toString()
+Array.prototype.unshift()
+Array.prototype.values()
+Array.prototype.with()
+ */
